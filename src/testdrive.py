709d6590b07adbf12890c77fdf035aa14a834139
Thomas Poiesz <ta.poiesz@gmail.com>
2017-01-23 20:59:20 +0100

    bugfix in first program

--- src/testdrive.py
+++ src/testdrive.py
@@ -3,5 +3,5 @@ import random
 roll = True
 
 while roll:
-    print(random.randrange(1, 6))
+    print(random.randint(1, 6))
     roll = input('Would you like to roll the dice again?(yes/no)') == 'yes'
